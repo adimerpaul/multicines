40e4e5ddf0b570817652c01c01117c114f98e239
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-08-12 23:28:47 -0400

    inicio impresion

--- front/src/pages/ListaVenta.vue
+++ front/src/pages/ListaVenta.vue
@@ -25,6 +25,8 @@
           </template>
           <template v-slot:body-cell-Opciones="props">
             <q-td :props="props" auto-width>
+              <q-btn icon="print" @click="printFactura(props.row)"/>
+
               <q-btn type="a" label="CLick" target="_blank" :href="`https://pilotosiat.impuestos.gob.bo/consulta/QR?nit=329448023&cuf=${props.row.cuf}&numero=${props.row.numeroFactura }&t=2`" />
             </q-td>
           </template>
@@ -40,7 +42,33 @@
       </div>
     </div>
     <div id="myelement">
-      sjkdsada
+      <div class="titulo">FACTURA <br>CON DERECHO A CREDITO FISCAL</div>
+      <div class="titulo2">MULTISALAS<br>
+        Casa Matriz<br>
+        No. Punto de Venta 0<br>
+        AVENIDA TACNA ENTRE TOMAS FRIAS Y JAEN<br>
+        Nª2337 ZONA ESTE<br>
+        Tel. 2846005<br>
+        Oruro
+      </div>
+      <hr>
+      <div class="titulo">NIT</div>
+      <div class="titulo2">329448023</div>
+      <div class="titulo">FACTURA N°</div>
+      <div class="titulo2">2</div>
+      <div class="titulo">CÓD. AUTORIZACIÓN</div>
+      <div class="titulo2 col-6" >168AB0075CF0FEC12741DDF938B3BBB63C339F8D472FEC0658B8B6D74</div>
+      <hr>
+      <table>
+        <tr><td class="titder">NOMBRE/RAZÓN SOCIAL:</td><td class="contenido">JUAN PEREZ</td></tr>
+        <tr><td class="titder">NIT/CI/CEX:</td><td class="contenido">1010</td></tr>
+        <tr><td class="titder">COD. CLIENTE:</td ><td class="contenido">4</td></tr>
+        <tr><td class="titder">FECHA DE EMISIÓN:</td><td class="contenido">12/08/2022 08:35 PM</td></tr>
+      </table>
+      <hr>
+      <div class="titulo">DETALLE</div>
+
+
     </div>
   </q-page>
 </template>
@@ -55,6 +83,7 @@ export default {
       fechaIni:date.formatDate(new Date(),'YYYY-MM-DD'),
       fechaFin:date.formatDate(new Date(),'YYYY-MM-DD'),
       listaVentaBoleteria:[],
+      facturadetalle:{},
       listaColums:[
         {name:'Opciones',label:'Opciones',field:'Opciones'},
         {name:'numeroFactura',label:'numeroFactura',field:'numeroFactura',sortable:true},
@@ -73,16 +102,47 @@ export default {
     this.listaVentaBoleteriaGet();
   },
   methods: {
-    printFactura(){
+    printFactura(factura){
+      console.log(factura)
+      this.facturadetalle=factura
       const cssText = `
+      .titulo{
+      font-size: 12px;
+      text-align: center;
+      font-weight: bold;
+      }
+      .titulo2{
+      font-size: 10px;
+      text-align: center;
+      }
+            .titulo3{
+      font-size: 10px;
+      text-align: center;
+      width:70%;
+      }
+            .contenido{
+      font-size: 10px;
+      text-align: left;
+      }
+      .titder{
+      font-size: 12px;
+      text-align: right;
+      font-weight: bold;
+      }
+      hr{
+  border-top: 1px dashed   ;
+}
+  table{
+    width:100%
+  }
   h1 {
     color: black;
     font-family: sans-serif;
   }
 `
-
+      //
       const d = new Printd()
-      d.print( document.getElementById('myelement'), [ cssText ] )
+      d.print( document.getElementById('myelement'),[cssText] )
     },
     listaVentaBoleteriaGet() {
       this.loading= true
