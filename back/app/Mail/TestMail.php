767eb70b08b200ec7a76f8cfb4ff410a357342a2
adimerpaul <adimer101@gmail.com>
2022-09-16 10:33:50 -0400

    cambiadno qr

--- back/app/Mail/TestMail.php
+++ back/app/Mail/TestMail.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Luecano\NumeroALetras\NumeroALetras;
-
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 class TestMail extends Mailable
 {
     use Queueable, SerializesModels;
