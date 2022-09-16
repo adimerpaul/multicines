8b4ad987c53d59ba3f99f97643c990a2b485ee15
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-09-16 11:13:20 -0400

    menu

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -59,6 +59,7 @@
 
         <q-expansion-item dense exact expand-separator icon="o_home_work" label="Factura de Alquiler " to="rental" expand-icon="null"/>
         <q-expansion-item dense exact expand-separator icon="o_local_activity" label="Prevalorada " to="prevalorada" expand-icon="null"/>
+        <q-expansion-item dense exact expand-separator icon="o_people" label="Clientes" to="cliente" expand-icon="null"/>
       </q-list>
     </q-drawer>
 
